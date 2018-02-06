8700175e3870ba3741d7fe001ceb981c9553d9f6
Pedrobusou <pedrobusou@gmail.com>
2018-02-07 00:05:31 +0100

    5: RoadGenerator generates new road sections, making the road infinite

--- Assets/scripts/RoadGenerator.cs
+++ Assets/scripts/RoadGenerator.cs
@@ -6,15 +6,22 @@ public class RoadGenerator : MonoBehaviour {
     public GameObject roadContainerGO;
     public GameObject[] roads;
 
+    public bool engineOn = false;
     public float speed = 15;
-    public bool engineOn;
 
     public int roadCounter = 0;
     public int roadNumberSelector;
 
+    public GameObject prevRoad;
+    public float prevRoadSize;
+
+    public Vector3 screenLimit;
+    public bool outOfScreen = true;
+
     void Start() {
         searchRoads();
         generateRoad();
+        getScreenSize();
     }
 
     void searchRoads() {
@@ -24,24 +31,27 @@ public class RoadGenerator : MonoBehaviour {
         for (int i = 0; i < roads.Length; i++) {
             roads[i].gameObject.transform.parent = roadContainerGO.transform;
             roads[i].gameObject.SetActive(false);
-            roads[i].gameObject.name = "RoadOFF_" + i;
+            roads[i].gameObject.name = "roadTemplate_" + i;
         }
     }
 
     void generateRoad() {
+        roadCounter++;
         roadNumberSelector = Random.Range(0, roads.Length);
         GameObject newRoad = Instantiate(roads[roadNumberSelector]);
         newRoad.SetActive(true);
-        newRoad.name = "Road_" + roadCounter;
+        newRoad.name = "road_" + roadCounter;
         newRoad.transform.parent = gameObject.transform;
-        roadCounter++;
 
         setRoadPosition(newRoad);
     }
 
     void setRoadPosition(GameObject newRoad) {
-        GameObject prevRoad = GameObject.Find("Road_" + (roadCounter - 1));
-        newRoad.transform.position = new Vector3(prevRoad.transform.position.x, prevRoad.transform.position.y + getRoadSize(prevRoad), 0);
+        prevRoad = GameObject.Find("road_" + (roadCounter - 1));
+        prevRoadSize = getRoadSize(prevRoad);
+        newRoad.transform.position = new Vector3(prevRoad.transform.position.x, prevRoad.transform.position.y + prevRoadSize, 0);
+
+        outOfScreen = false;
     }
 
     float getRoadSize(GameObject road) {
@@ -52,9 +62,19 @@ public class RoadGenerator : MonoBehaviour {
         return roadSize;
     }
 
+    void getScreenSize() {
+        screenLimit = new Vector3(0, GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(0, 0, 0)).y - 0.5f, 0);
+    }
+
     void Update() {
         if (engineOn) {
             transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+
+        if (!outOfScreen && prevRoad.transform.position.y + prevRoadSize < screenLimit.y) {
+            outOfScreen = true;
+            Destroy(prevRoad);
+            generateRoad();
         }
     }
 }
