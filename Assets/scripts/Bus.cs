8b030cddf28ede6e5dac1020c73adcb405dc04f6
Pedrobusou <pedrobusou@gmail.com>
2018-02-10 21:11:42 +0100

    9: Added Bus and crash mechanic

---
+++ Assets/scripts/Bus.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bus : MonoBehaviour {
+	public GameObject chronometerGO;
+	public Chronometer chronometerScript;
+
+	public GameObject soundFxGO;
+	public SoundFx soundFxScript;
+
+	void Start() {
+		chronometerGO = GameObject.FindObjectOfType<Chronometer>().gameObject;
+		chronometerScript = chronometerGO.GetComponent<Chronometer>();
+
+		soundFxGO = GameObject.FindObjectOfType<SoundFx>().gameObject;
+		soundFxScript = soundFxGO.GetComponent<SoundFx>();
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.GetComponent<Car>()) {
+			soundFxScript.playCrashFx();
+			chronometerScript.time = chronometerScript.time - 20;
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/scripts/SoundFx.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFx : MonoBehaviour {
+	public AudioClip[] soundFx;
+	public AudioSource audioS;
+
+	void Start() {
+		audioS = GetComponent<AudioSource>();
+	}
+
+	public void playCrashFx() {
+		audioS.clip = soundFx[0];
+		audioS.Play();
+	}
+
+	public void playEndMusic() {
+		audioS.clip = soundFx[1];
+		audioS.Play();
+	}
+}
