3775521a1aa9d33b40b33177397088aa3da8fa81
Pedrobusou <pedrobusou@gmail.com>
2018-02-10 19:12:47 +0100

    8: Add controls to car, turn movement and limitWalls

---
+++ Assets/scripts/CarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+	public GameObject carGO;
+
+	public float turnAngle = 45;
+	public float speed = 25;
+
+	void Start() {
+		carGO = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update() {
+		float turnZ = 0;
+		transform.Translate(Vector2.right * Input.GetAxis("Horizontal")* speed * Time.deltaTime);
+		turnZ = Input.GetAxis("Horizontal")* -turnAngle;
+
+		carGO.transform.rotation = Quaternion.Euler(0, 0, turnZ);
+	}
+}
